898f5a3e5c86dcb778177a78696fe302946c40c3
Momoroy <64591496+Momoroy@users.noreply.github.com>
2020-11-26 16:19:44 +0100

    Add files via upload

--- Btree.java
+++ Btree.java
@@ -1,87 +1,123 @@
-package movida.commons;
 
-public class Btree {
-
-    public BtreeNode root; // Pointer to root node
-        public int t; // Minimum degree
-
-        // Constructor (Initializes tree as empty)
-        Btree(int t) {
-            this.root = null;
-            this.t = t;
-        }
-
-
-        // function to search a key in this tree
-        public BtreeNode search (String k) {
-            if (this.root == null)
-                return null;
-            else
-                return this.root.search(k);
-        }
-
-        public void insert(Movie key) {
-            boolean leaf;
-
-            if (root == null) {
-                root = new BtreeNode(t);
-                root.insertAsSorted(key);
-                return;
+public class Btree<K extends Comparable<K>, V> implements IDictionary<K,V>{
+    private BtreeNode root;             // radice del Btree
+    private class BtreeNode {
+        private int size;          // Numero di Nodi
+        private K key;           // Chiavi
+        private V value;         // Dato della chiave
+        private BtreeNode left, right;  // Alberi a destra e sinistra
+        
+        public BtreeNode(int size, K key, V value) {
+            this.size = size;
+            this.key = key;
+            this.value = value;
             }
-            BtreeNode parent = null;
-            BtreeNode node = root;
-            int childIndex = 0;
+    }
+    public Btree() {
+    }
 
-            while (true) {
+    public boolean isEmpty(BtreeNode x) {
+        return size(x) == 0;    //Restituisce vero se non contiente alcun valore
+    }
 
-                //Proactive Split
-                if (node.isFull()) {
-                    if (parent == null) {
-                        parent = new BtreeNode(t);
-                        root = parent;
-                    }
-                    parent.splitChild(childIndex, node);
-                    if((key.getTitle()).compareTo((parent.a[childIndex].getTitle()))==0){
-                        //Duplicate Data rejected
-                        return;
-                    }
 
-                    if ((key.getTitle()).compareTo((parent.a[childIndex].getTitle())) < 0) {
-                        node = parent.c[childIndex];
-                    } else {
-                        node = parent.c[childIndex+1];
-                    }
-                }
-                //Binary Search
-                int left = 0, right = node.n - 1;
-                while (left <= right) {
-                    int mid = left + (right - left) / 2;
+    
+    private int size(BtreeNode x) { 
+        if (x == null) return 0;  
+        else return x.size;   //Restituisce la grandezza del nodo
+    }
 
-                    if ((key.getTitle()).compareTo((node.a[mid].getTitle())) == 0) {
-                        //Duplicate Data rejected
-                        //For upsert, we can update the value of the key.
-                        return;
-                    }
+    private BtreeNode min(BtreeNode x) {  //Restituisce la chiave minore
+        if (x.left == null) 
+        return x; 
+        else                
+        return min(x.left);
+    }
 
-                    if ((key.getTitle()).compareTo((node.a[mid].getTitle())) < 0) {
-                        right = mid - 1;
-                    } else {
-                        left = mid + 1;
-                    }
-                }
-
-                // If not found, traverse down the children
-                if (node.isLeaf()) {
-                    break;
-                }
-                parent = node;
-                childIndex = right+1;
-                node = node.c[childIndex];
-            }
-
-            //Insert into the leaf node.
-            node.insertAsSorted(key);
-        }
+    @Override
+    public V search(K key) {
+        if(key == null)
+        throw new IllegalArgumentException("calls search() with a null key");
+         return search(root, key);
 
     }
+
+    private V search(BtreeNode x, K key){    
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0)
+        return x.value;
+        else if (cmp < 0) 
+        return search(x.left,  key);
+        else if (cmp > 0) 
+        return search(x.right, key);
+        else
+        return x.value;
+    }
+
+    public void insert(K key, V val) {
+        if (key == null) throw new IllegalArgumentException("calls put() with a null key");
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        root = insert(root, key, val);
+    }
+
+    private BtreeNode insert(BtreeNode x, K key, V val) {
+        if (x == null) 
+        return new BtreeNode(1, key, val);
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) 
+        x.left  = insert(x.left,  key, val);
+        else if (cmp > 0) 
+        x.right = insert(x.right, key, val);
+        else              
+        x.value   = val;
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    private BtreeNode deleteMin(BtreeNode x) {  //Rimuove la chiave minore presente nel nodo
+        if (x.left == null) 
+        return x.right;
+        x.left = deleteMin(x.left);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+   
+    @Override
+    public V delete(K key) {
+        if (key == null) 
+        throw new IllegalArgumentException("calls delete() with a null key");
+        root = delete(root, key);
+        return root.value;
+    }
+
+
+    private BtreeNode delete(BtreeNode x, K key) {
+        if (x == null) 
+        return null;
+
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) 
+        x.left  = delete(x.left,  key);
+        else if (cmp > 0) 
+        x.right = delete(x.right, key);
+        else {
+            if (x.right == null) return x.left;
+            if (x.left  == null) return x.right;
+            BtreeNode t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    
+
+}
+
 
