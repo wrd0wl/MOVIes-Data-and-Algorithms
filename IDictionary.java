e581740267548860c89b4eab81535d6c5383daf0
wrd0wl <36873401+wrd0wl@users.noreply.github.com>
2021-02-18 02:40:28 +0100

    Delete IDictionary.java

--- IDictionary.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface IDictionary <K extends Comparable<K>, V> {
-    void insert (K key, V value);
-    V search (K key);
-    void delete(K key);
-    void clear();
-    ArrayList<V> keyValues();
-}
