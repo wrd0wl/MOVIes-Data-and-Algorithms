7a502fa26eb29500aa90cfbf5d561c49d7c5f17e
wrd0wl <36873401+wrd0wl@users.noreply.github.com>
2021-02-19 22:45:50 +0100

    Completed

--- src/implementation/ScoreComparator.java
+++ src/implementation/ScoreComparator.java
@@ -6,7 +6,6 @@ import java.util.Comparator;
 
 public class ScoreComparator implements Comparator<Collaboration> {
 
-    @Override
     public int compare(Collaboration m1, Collaboration m2) {
         return m2.getScore().compareTo(m1.getScore());
     }
